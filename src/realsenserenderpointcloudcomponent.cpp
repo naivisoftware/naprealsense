d23bc54155abeaf4b7d8e685f0b6e3b51871d8a6
Tim Groeneboom <tim@timgroeneboom.com>
2022-09-21 11:14:00 +0200

    - RealSenseRenderPointCloudComponent working

--- src/realsenserenderpointcloudcomponent.cpp
+++ src/realsenserenderpointcloudcomponent.cpp
@@ -1,18 +1,96 @@
 #include "realsenserenderpointcloudcomponent.h"
 #include "realsensedevice.h"
+#include "renderglobals.h"
+
+#include <renderservice.h>
+#include <meshutils.h>
 
 // RealSense includes
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 
 RTTI_BEGIN_CLASS(nap::RealSenseRenderPointCloudComponent)
+        RTTI_PROPERTY("Mesh", &nap::RealSenseRenderPointCloudComponent::mMesh, nap::rtti::EPropertyMetaData::Required)
 RTTI_END_CLASS
 
 RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::RealSenseRenderPointCloudComponentInstance)
     RTTI_CONSTRUCTOR(nap::EntityInstance&, nap::Component&)
 RTTI_END_CLASS
 
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::PointCloudMesh)
+    RTTI_CONSTRUCTOR(nap::Core&)
+RTTI_END_CLASS
+
 namespace nap
 {
+    PointCloudMesh::PointCloudMesh(Core& core) :
+        mRenderService(core.getService<RenderService>())
+    { }
+
+    bool PointCloudMesh::init(utility::ErrorState& errorState)
+    {
+        // Setup plane
+        if (!setup(errorState))
+            return false;
+
+        // Initialize instance
+        return mMeshInstance->init(errorState);
+    }
+
+    bool PointCloudMesh::setup(utility::ErrorState& error)
+    {
+        // Construct bounding rect
+        float dsizex = (0.0f - (mSize.x / 2.0f)) + mPosition.x;
+        float dsizey = (0.0f - (mSize.y / 2.0f)) + mPosition.y;
+        math::Rect rect(dsizex, dsizey, mSize.x, mSize.y);
+
+        // Create plane
+        assert(mRenderService != nullptr);
+        mMeshInstance = std::make_unique<MeshInstance>(*mRenderService);
+
+        constructPointCloud(*mMeshInstance, 100000);
+
+        // Store rect
+        mRect = rect;
+
+        return true;
+    }
+
+
+    void PointCloudMesh::constructPointCloud(nap::MeshInstance& mesh, size_t vertCount)
+    {
+        // Get incremental stepping values
+        std::vector<glm::vec3> vertices(vertCount, { 0.0f,0.0f,0.0f });
+        std::vector<glm::vec3> normals(vertCount, { 0.0f,0.0f,1.0f });
+        std::vector<glm::vec3> uvs(vertCount, { 0.0f,0.0f,0.0f });
+
+        Vec3VertexAttribute& position_attribute = mesh.getOrCreateAttribute<glm::vec3>(vertexid::position);
+        Vec3VertexAttribute& normal_attribute = mesh.getOrCreateAttribute<glm::vec3>(vertexid::normal);
+        Vec3VertexAttribute& uv_attribute = mesh.getOrCreateAttribute<glm::vec3>(vertexid::getUVName(0));
+        Vec4VertexAttribute& color_attribute = mesh.getOrCreateAttribute<glm::vec4>(vertexid::getColorName(0));
+
+        // Set the number of vertices to use
+        mesh.setNumVertices(vertCount);
+        mesh.setDrawMode(EDrawMode::Points);
+        mesh.setUsage(mUsage);
+        mesh.setCullMode(mCullMode);
+        mesh.setPolygonMode(mPolygonMode);
+
+        // Push vertex data
+        position_attribute.setData(vertices.data(), vertCount);
+        normal_attribute.setData(normals.data(), vertCount);
+        uv_attribute.setData(uvs.data(), vertCount);
+        color_attribute.setData({vertCount, mColor.toVec4()});
+
+        std::vector<uint32> indices(vertCount, 0);
+        for(size_t i = 0; i < vertCount; i++)
+        {
+            indices[i] = i;
+        }
+
+        MeshShape& shape = mesh.createShape();
+        shape.setIndices(indices.data(), indices.size());
+    }
+
     struct RealSenseRenderPointCloudComponentInstance::Impl
     {
     public:
@@ -37,6 +115,28 @@ namespace nap
     }
 
 
+    bool RealSenseRenderPointCloudComponentInstance::onInit(utility::ErrorState& errorState)
+    {
+        auto* resource = getComponent<RealSenseRenderPointCloudComponent>();
+        mMesh = resource->mMesh.get();
+
+        frameSetReceived.connect([this](const rs2::frameset& frameset){ onTrigger(frameset); });
+
+        return true;
+    }
+
+
+    void RealSenseRenderPointCloudComponentInstance::update(double deltaTime)
+    {
+        if(mDirty)
+        {
+
+
+            mDirty = false;
+        }
+    }
+
+
     void RealSenseRenderPointCloudComponentInstance::onTrigger(const rs2::frameset& frameset)
     {
         auto color = frameset.get_color_frame();
@@ -53,5 +153,41 @@ namespace nap
         // Generate the pointcloud and texture mappings
         mImplementation->mPoints = mImplementation->mPointCloud.calculate(depth);
 
+        mDirty = true;
+
+        auto& mesh_instance = mMesh->getMeshInstance();
+
+        if(mImplementation->mPoints.size()!=mesh_instance.getNumVertices())
+        {
+            mMesh->constructPointCloud(mesh_instance, mImplementation->mPoints.size());
+        }
+
+        auto& vertex_attribute = mesh_instance.getAttribute<glm::vec3>(vertexid::position);
+        auto& uv_attribute = mesh_instance.getAttribute<glm::vec3>(vertexid::uv);
+        auto& color_attribute = mesh_instance.getAttribute<glm::vec4>(vertexid::color);
+        auto& vertex_positions = vertex_attribute.getData();
+        auto& tex_coords = uv_attribute.getData();
+        auto& colors = color_attribute.getData();
+        auto* pc_vertices   = mImplementation->mPoints.get_vertices();              // get vertices
+        auto* pc_tex_coords = mImplementation->mPoints.get_texture_coordinates(); // and texture coordinates
+        for(size_t i = 0; i < mImplementation->mPoints.size(); i++)
+        {
+            if(pc_vertices[i].z)
+            {
+                glm::vec3 p = glm::vec3(pc_vertices[i].x, pc_vertices[i].y, pc_vertices[i].z);
+                vertex_positions[i] = p;
+                tex_coords[i] = glm::vec3(pc_tex_coords[i].u, pc_tex_coords[i].v, 0.0f);
+                colors[i] = glm::vec4(1,1,1,1);
+            }else
+            {
+                colors[i] = glm::vec4(0,0,0,0);
+            }
+        }
+
+        utility::ErrorState errorState;
+        if(!mesh_instance.update(errorState))
+        {
+            nap::Logger::warn(errorState.toString());
+        }
     }
 }
--- src/realsenserenderpointcloudcomponent.h
+++ src/realsenserenderpointcloudcomponent.h
@@ -1,16 +1,76 @@
 #pragma once
 
+#include <mesh.h>
+#include <rect.h>
+
 #include "realsenseframesetlistenercomponent.h"
 
 namespace nap
 {
     class RealSenseRenderPointCloudComponentInstance;
+    class RenderService;
+
+    /**
+	 */
+    class NAPAPI PointCloudMesh : public IMesh
+    {
+    RTTI_ENABLE(IMesh)
+    public:
+        PointCloudMesh(Core& core);
+
+        /**
+         * Sets up, initializes and uploads the plane to the GPU based on the provided parameters.
+         * @param errorState contains the error message if the mesh could not be created.
+         * @return if the mesh was successfully created and initialized.
+         */
+        virtual bool init(utility::ErrorState& errorState) override;
+
+        /**
+        * Creates and prepares the mesh instance but doesn't initialize it.
+        * Call this when you want to prepare a grid without creating the GPU representation.
+        * You have to manually call init() on the mesh instance afterwards.
+        * @param error contains the error code if setup fails
+        * @return if setup succeeded
+        */
+        bool setup(utility::ErrorState& error);
+
+        /**
+         *	@return the mesh used for rendering
+         */
+        virtual MeshInstance& getMeshInstance() override					{ return *mMeshInstance; }
+
+        /**
+         *	@return the mesh used for rendering
+         */
+        virtual const MeshInstance& getMeshInstance() const override		{ return *mMeshInstance; }
+
+        /**
+         *	@return the plane as a rectangle
+         */
+        const math::Rect& getRect()											{ return mRect; }
+
+        // property: the size of the plane
+        glm::vec2		mSize			= { 1.0, 1.0 };						///< Property: 'Size' the size of the plane in units
+        glm::vec2		mPosition		= { 0.0,0.0 };						///< Property: 'Position' where the plane is positioned in object space
+        RGBAColorFloat	mColor			= { 1.0f, 1.0f, 1.0f, 1.0f };		///< Property: 'Color' color of the plane
+        EMemoryUsage	mUsage			= EMemoryUsage::DynamicWrite;		///< Property: 'Usage' If the plane is uploaded once or frequently updated.
+        ECullMode		mCullMode		= ECullMode::None;					///< Property: 'CullMode' Plane cull mode, defaults to no culling
+        EPolygonMode	mPolygonMode	= EPolygonMode::Point;				///< Property: 'PolygonMode' Polygon rasterization mode (fill, line, points)
+
+        void constructPointCloud(nap::MeshInstance& mesh, size_t vertCount);
+
+    private:
+        RenderService* mRenderService;
+        std::unique_ptr<MeshInstance> mMeshInstance;
+        math::Rect mRect;
+    };
 
     class NAPAPI RealSenseRenderPointCloudComponent : public RealSenseFrameSetListenerComponent
     {
     RTTI_ENABLE(RealSenseFrameSetListenerComponent)
     DECLARE_COMPONENT(RealSenseRenderPointCloudComponent, RealSenseRenderPointCloudComponentInstance)
     public:
+        ResourcePtr<PointCloudMesh> mMesh;
     };
 
     class NAPAPI RealSenseRenderPointCloudComponentInstance : public RealSenseFrameSetListenerComponentInstance
@@ -21,9 +81,16 @@ namespace nap
 
         virtual ~RealSenseRenderPointCloudComponentInstance();
     protected:
+        bool onInit(utility::ErrorState& errorState) override;
+
         void onTrigger(const rs2::frameset& frameset);
+
+        void update(double deltaTime) override;
     private:
         struct Impl;
         std::unique_ptr<Impl> mImplementation;
+
+        PointCloudMesh* mMesh;
+        bool mDirty = false;
     };
 }
