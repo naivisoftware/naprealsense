295fe36145f9fdd64c90e5c79e3c3fbfd6cb9333
DESKTOP-960O4D7\Tim Groeneboom <tim@timgroeneboom.com>
2022-10-20 08:55:08 +0200

    - moved cmake files to thirdparty and module space

--- dist/cmake/module_extra.cmake
+++ dist/cmake/module_extra.cmake
@@ -1,10 +1,13 @@
 include(${NAP_ROOT}/cmake/dist_shared_crossplatform.cmake)
 
 if(NOT TARGET realsense)
-    find_package(realsense REQUIRED)
+    find_package(realsense PATHS ${THIRDPARTY_DIR}/librealsense/cmake REQUIRED)
 endif()
 
 set(MODULE_NAME_EXTRA_LIBS realsense)
+
+# add includes
+target_include_directories(${PROJECT_NAME} PUBLIC ${THIRDPARTY_DIR}/librealsense/include)
 
 if(WIN32)
     # Add post-build step to set copy realsense to bin
@@ -13,8 +16,7 @@ if(WIN32)
             COMMAND ${CMAKE_COMMAND}
             -E copy
             $<TARGET_FILE:realsense>
-            $<TARGET_FILE_DIR:${PROJECT_NAME}>
-            )
+            $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 elseif(UNIX)
     # Install realsense lib into packaged project
     file(GLOB REALSENSE_DYLIBS ${THIRDPARTY_DIR}/librealsense/bin/librealsense2*${CMAKE_SHARED_LIBRARY_SUFFIX}*)
